25233464d906b273110387e048cc32694abdb571
elnexreal <elnexreal@DESKTOP-DMCGJUB>
2023-02-05 20:55:10 -0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Http;
+using System.Windows;
+
+namespace GotYourWebhook
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		HttpClient client = new HttpClient();
+
+		public async void DeleteWebhook(object sender, RoutedEventArgs e)
+		{
+			string url = WebhookUrl.Text;
+
+			if (Uri.IsWellFormedUriString(url, UriKind.Absolute))
+			{
+				try
+				{
+					using (HttpResponseMessage response = await client.DeleteAsync(url))
+					{
+						if (response.StatusCode == System.Net.HttpStatusCode.OK)
+						{
+							MessageBox.Show("Webhook successfully deleted");
+						}
+					}
+				}
+				catch (HttpRequestException err)
+				{
+					MessageBox.Show($"Error: {err.Message}");
+				}
+			} else
+			{
+				MessageBox.Show("Enter a valid URL");
+			}
+		}
+
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+	}
+}
